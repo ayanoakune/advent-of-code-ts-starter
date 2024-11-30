89e189fe08a37b7475080f4d8782d5448db9f3dd
akune <ayanoakune@gmail.com>
2024-11-30 17:04:23 +0200

    Fix devtools enabling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
 
   devtools: {
-    enabled: import.meta.dev,
+    enabled: "development" === process.env.NODE_ENV,
   },
 
   modules: ["@nuxt/ui"],
