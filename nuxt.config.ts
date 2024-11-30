a7bb0f1d960839fa09b44b33d61e17d3359c1a6f
akune <ayanoakune@gmail.com>
2024-11-30 13:59:49 +0200

    Add @nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+
   devtools: {
     enabled: import.meta.dev,
   },
+
+  modules: ["@nuxt/ui"],
 });
