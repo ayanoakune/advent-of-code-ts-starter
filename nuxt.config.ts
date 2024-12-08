a60adb7e26e994669b28864f8a8b525a11c66c3a
akune <ayanoakune@gmail.com>
2024-12-08 12:14:44 +0200

    Update Nuxt root element style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-
+  app: {
+    rootAttrs: {
+      class: "min-h-screen",
+    },
+  },
   devtools: {
     enabled: "development" === process.env.NODE_ENV,
   },
-
   modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"],
 });
